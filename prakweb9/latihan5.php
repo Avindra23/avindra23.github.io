cba3f4717de80ab44f628d17b7f42485f74dc391
Avindra23 <149358654+Avindra23@users.noreply.github.com>
2023-12-30 21:24:31 +0700

    Praktikum Modul 9

---
+++ prakweb9/latihan5.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>LATIHAN 5 PEMROGRAMAN WEB</title>
+    </head>
+    <body>
+        <?php
+            $nim = "2105101006";
+            $nama = "Avindra Pradevipti"; 
+            $angka1 = 23; 
+            $angka2 = 12;
+
+            $hasilpenjumlahan = $angka1 + $angka2;
+
+            echo "NIM : ". $nim . "<br/>";
+            echo "NAMA : ". $nama . "<br/>";
+            echo "Hasil Penjumlahan dari : " . $angka1 ." + ". $angka2 . " = " . $hasilpenjumlahan . "<br/>";
+        ?>
+    </body>
+</html>
